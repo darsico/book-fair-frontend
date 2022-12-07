483569cbc763df8a168b3e80be132c279a6badef
darsico <diegojairhl@gmail.com>
2022-12-06 19:33:06 -0500

    fix: seller api validation fixed

--- src/api/sellerApi.js
+++ src/api/sellerApi.js
@@ -9,37 +9,40 @@ const sellerApi = axios.create({
 });
 
 export const getSellers = async () => {
- const { data } = await sellerApi.get('/sellers');
- return data;
-}
+ try {
+  const { data } = await sellerApi.get('/sellers');
+  return data;
+ } catch (error) {
+  console.log(error);
+ }
+};
 
 export const getSellerById = async (sellerId) => {
  const token = localStorage.getItem('token');
  try {
   const { data } = await sellerApi.get(`/seller/${sellerId}`, {
    headers: {
-    "x-access-token": `${token}`
-   }
+    'x-access-token': `${token}`,
+   },
   });
-  return data
+  return data;
  } catch (error) {
-  console.log(error,
-  );
+  console.log(error);
  }
-}
+};
 
 export const createSeller = async (sellerData) => {
  try {
   const { data } = await sellerApi.post('/auth/new', sellerData);
   return data;
  } catch (error) {
-  console.log(error)
+  console.log(error);
   document.getElementById('error').innerHTML = error.response.data.message;
   console.log({
    message: error.response.data.message,
   });
  }
-}
+};
 
 export const loginSeller = async (sellerData) => {
  try {
@@ -51,60 +54,57 @@ export const loginSeller = async (sellerData) => {
    message: error.response.data.message,
   });
  }
-}
+};
 
 export const getSellerBooksPublic = async (sellerId) => {
  const { data } = await sellerApi.get(`/sellers/${sellerId}/books`);
 
  return data;
-}
+};
 
 export const getSellerBooks = async (sellerId) => {
  const token = localStorage.getItem('token');
  try {
   const { data } = await sellerApi.get(`/seller/${sellerId}/books`, {
    headers: {
-    "x-access-token": `${token}`
-   }
+    'x-access-token': `${token}`,
+   },
   });
-  return data
+  return data;
  } catch (error) {
   console.log(error);
  }
-}
+};
 
 export const createBook = async (sellerId, bookData) => {
  const token = localStorage.getItem('token');
  try {
   const { data } = await sellerApi.post(`/seller/${sellerId}/books/new`, bookData, {
    headers: {
-    "x-access-token": `${token}`
-   }
+    'x-access-token': `${token}`,
+   },
   });
   return data;
  } catch (error) {
   console.log({
-   message: error.response.data.message || "Error when creating book",
+   message: error.response.data.message || 'Error when creating book',
   });
   document.getElementById('error').innerHTML = error.response.data.message;
-
  }
-}
+};
 
 export const getSellerData = async (sellerId) => {
  const token = localStorage.getItem('token');
  try {
   const { data } = await sellerApi.get(`/seller/${sellerId}/`, {
    headers: {
-    "x-access-token": `${token}`
-   }
+    'x-access-token': `${token}`,
+   },
   });
-  return data
+  return data;
  } catch (error) {
   console.log(error);
  }
-}
+};
 
-
-
-export default sellerApi
+export default sellerApi;
--- src/pages/sellers.jsx
+++ src/pages/sellers.jsx
@@ -1,22 +1,22 @@
-import { useQuery } from "react-query";
-import { Link, useSearchParams } from "react-router-dom";
-import { getSellers } from "../api/sellerApi";
-import Book from "../components/Book/Book";
-import SpinnerSection from "../components/Loader/SpinnerSection";
-import Search from "../components/Search/Search";
+import { useQuery } from 'react-query';
+import { Link, useSearchParams } from 'react-router-dom';
+import { getSellers } from '../api/sellerApi';
+
+import SpinnerSection from '../components/Loader/SpinnerSection';
+import Search from '../components/Search/Search';
 
 const Sellers = () => {
- const { data, isLoading, error } = useQuery('seller', getSellers);
- const [params, setParams] = useSearchParams()
+ const { data, isLoading, isError, error } = useQuery('seller', getSellers);
+ const [params, setParams] = useSearchParams();
  if (isLoading) return <SpinnerSection />;
- if (error) return <h1>Something went wrong</h1>;
- if (data?.length === 0) return <h1>No sellers yet</h1>
+ if (isError) return <h1>{error.message}</h1>;
+ if (!data) return <h1>No sellers yet</h1>;
+
  const { sellers } = data;
 
  const searchTerms = params.get('title') || '';
 
- const filteredSellers = sellers?.filter((seller) => seller.store.toLowerCase().includes(searchTerms?.toLowerCase()))
-
+ const filteredSellers = sellers?.filter((seller) => seller.store.toLowerCase().includes(searchTerms?.toLowerCase()));
 
  return (
   <section className="page-container">
@@ -26,18 +26,21 @@ const Sellers = () => {
    </div>
    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4  gap-10">
     {filteredSellers.map((seller) => (
-     <div key={seller._id} className="p-2 border border-solid " >
+     <div key={seller._id} className="p-2 border border-solid ">
       <h2 className="font-special font-bold text-base">{seller.store}</h2>
-      <p className="text-sm ">Owner:<span className="text-gray-600"> {seller.name}</span></p>
+      <p className="text-sm ">
+       Owner:<span className="text-gray-600"> {seller.name}</span>
+      </p>
       <p className="text-sm ">Email: {seller.email}</p>
-      <p>{seller.books.length >= 1 ? `${seller.books.length} books` : "No books yet"}</p>
-      <Link to={`/sellers/${seller._id}`}><button className="text-lg mt-4 underline">Go to store</button></Link>
+      <p>{seller.books.length >= 1 ? `${seller.books.length} books` : 'No books yet'}</p>
+      <Link to={`/sellers/${seller._id}`}>
+       <button className="text-lg mt-4 underline">Go to store</button>
+      </Link>
      </div>
     ))}
    </div>
   </section>
-
  );
-}
+};
 
 export default Sellers;
